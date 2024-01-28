61194d3bf2e0ccb78cf0c8994ed387614c239beb
Yaser M. Shabana <yaser.m.shabana@gmail.com>
2024-01-28 17:25:37 +0200

    Save data and generate HTML for payment Summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,5 @@
-import { renderOrderSummary } from "../scripts/Checkout/orderSummary.js";
+import { renderOrderSummary } from "./Checkout/orderSummary.js";
+import { renderPaymentSummary } from "./Checkout/PaymentSummary.js";
 
 renderOrderSummary();
+renderPaymentSummary();
