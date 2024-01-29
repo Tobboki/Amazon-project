69d4a92c794e1e45f67dbcec3a57d55ae2c40d1f
Yaser M. Shabana <yaser.m.shabana@gmail.com>
2024-01-29 13:10:10 +0200

    Create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,34 @@
+import formatCurrency from "../scripts/utils/money.js";
+
+console.log('test suite formatCurrency');
+
+console.log('converts cents into dollars');
+//passed
+if (formatCurrency(2095) === '20.95'){
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('works with zero');
+//passed
+if  (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('rounds up to the nearest cent');
+//passed
+if  (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+//passed
+if  (formatCurrency(2000.4) === '20.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
